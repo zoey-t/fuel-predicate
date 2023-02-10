4e8220aaa1b20abf04da8e7062264a2d6110d029
zoey-t <101599447+zoey-t@users.noreply.github.com>
2023-02-10 15:39:41 -0800

    cargo fmt to format code

--- tests/harness.rs
+++ tests/harness.rs
@@ -22,39 +22,54 @@ async fn test_simple_predicate() {
 
     // transferr funds to predicate
     let amount = 1234u64;
-    predicate.receive(&wallet1, amount, asset_id, None).await.ok(); // put .ok() to ignore used result (by making it an Option)
+    predicate
+        .receive(&wallet1, amount, asset_id, None)
+        .await
+        .ok(); // put .ok() to ignore used result (by making it an Option)
 
     // check predicate balance
     let provider = wallet1.get_provider().unwrap();
-    let predicate_balance = provider.get_asset_balance(predicate.address(), asset_id).await.unwrap();
+    let predicate_balance = provider
+        .get_asset_balance(predicate.address(), asset_id)
+        .await
+        .unwrap();
     println!("predicate balance{}:", predicate_balance);
     assert_eq!(predicate_balance, amount);
 
     // unlock predicate funds and send it to wallet2
-    let amount_to_unlock:u64 = 300;
+    let amount_to_unlock: u64 = 300;
     let balance_before = wallet2.get_asset_balance(&asset_id).await.unwrap();
 
-    predicate.encode_data(111,111).spend(&wallet2, amount_to_unlock, asset_id, None).await.ok();
+    predicate
+        .encode_data(111, 111)
+        .spend(&wallet2, amount_to_unlock, asset_id, None)
+        .await
+        .ok();
 
     let balance_after = wallet2.get_asset_balance(&asset_id).await.unwrap();
 
-    assert_eq!(balance_after-balance_before, amount_to_unlock);
+    assert_eq!(balance_after - balance_before, amount_to_unlock);
 
     // check predicate remaining balance
-    let predicate_balance = provider.get_asset_balance(predicate.address(), asset_id).await.unwrap();
+    let predicate_balance = provider
+        .get_asset_balance(predicate.address(), asset_id)
+        .await
+        .unwrap();
     println!("predicate new balance{}:", predicate_balance);
-    assert_eq!(predicate_balance, amount-amount_to_unlock);
+    assert_eq!(predicate_balance, amount - amount_to_unlock);
 
     // should fail if providing wrong data
-    let res = predicate.encode_data(1, 2).spend(&wallet2, amount_to_unlock, asset_id, None).await;
+    let res = predicate
+        .encode_data(1, 2)
+        .spend(&wallet2, amount_to_unlock, asset_id, None)
+        .await;
     match res {
         Ok(_receipt) => {
             println!("predicate unlocked by providing wrong data!!!")
-        },
+        }
         Err(err) => {
             println!("expected! predicate should not be unlocked with wrong data\n");
             println!("{:?}", err);
         }
     }
-    
 }
